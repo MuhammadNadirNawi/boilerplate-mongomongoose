3431f05e0071733661da6686045f9096df96a04a
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-01-04 18:11:39 +0800

    create document findEditandSave

--- myApp.js
+++ myApp.js
@@ -69,10 +69,17 @@ const findPersonById = (personId, done) => {
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-  Person.findById({ _id: personId }, function (err, result) {
+  Person.findById(personId, (err, person) => {
     if (err) return console.log(err);
-    let data = result.push(foodToAdd);
-    done(null, data);
+
+    // Array.push() method to add "hamburger" to the list of the person's favoriteFoods
+    person.favoriteFoods.push(foodToAdd);
+
+    // and inside the find callback - save() the updated Person.
+    person.save((err, updatedPerson) => {
+      if (err) return console.log(err);
+      done(null, updatedPerson);
+    });
   });
 };
 
