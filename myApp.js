1a01a10ca6166b33c7e3cfaaf9506adfe1243645
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-01-05 08:28:37 +0800

    add find

--- myApp.js
+++ myApp.js
@@ -108,8 +108,10 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch}.sort({name: 1}).limit(2).select("name favoriteFoods").exec((err,data)=> {
+    if (err) return console.log(err)
+    done(null, data);
+  }))
 };
 
 /** **Well Done !!**
